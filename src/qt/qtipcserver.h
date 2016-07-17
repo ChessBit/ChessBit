98d37d0f908c72cce843762029a289ae9a5b4655
COINFORCHESS <levuhuynh159@gmail.com>
2016-07-18 00:18:03 +0700

    ChessBit Source Codes
    ChessBit 2016

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define ChessBit-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "ChessBitURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
